9a229661e1d24c1af9a9b075523bf6ccdc2fd743
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-03 02:06:15 +0800

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -3,9 +3,6 @@
 # Update the configuration file
 sed -i "s@listenGrpcMultiaddr: \"\"@listenGrpcMultiaddr: /ip4/127.0.0.1/tcp/8337@" ceremonyclient/node/.config/config.yml
 
-# Install grpcurl
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
-
 # Build and install the Go application
 cd ceremonyclient/node
 GOEXPERIMENT=arenas /usr/local/go/bin/go install ./...

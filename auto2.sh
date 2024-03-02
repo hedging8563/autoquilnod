8a9d365f7de5d7eef2d50cdfb0d3f4b2b9f66121
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-03 01:54:35 +0800

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -7,7 +7,8 @@ sed -i "s@listenGrpcMultiaddr: \"\"@listenGrpcMultiaddr: /ip4/127.0.0.1/tcp/8337
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
 # Build and install the Go application
-cd ceremonyclient/node && GOEXPERIMENT=arenas go install ./...
+cd ceremonyclient/node
+GOEXPERIMENT=arenas /usr/local/go/bin/go install ./...
 
 # Create and start the systemd service
 sudo bash -c 'echo -e "[Unit]\nDescription=Ceremony Client Go App Service\n\n[Service]\nType=simple\nRestart=always\nRestartSec=5s\nWorkingDirectory=/root/ceremonyclient/node\nEnvironment=GOEXPERIMENT=arenas\nExecStart=/root/go/bin/node ./...\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/ceremonyclient.service'

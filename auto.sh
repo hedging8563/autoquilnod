ae559a621383d7a7d709fd7d9c2f6d701a9d63db
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-02 23:50:27 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -22,9 +22,7 @@ cd ceremonyclient/node && GOEXPERIMENT=arenas go run ./... # 进入 ceremonyclie
 sleep 180 # 等待程序运行 3 分钟
 pkill go # 终止所有正在运行的 Go 程序
 sed -i "s@listenGrpcMultiaddr: \"\"@listenGrpcMultiaddr: /ip4/127.0.0.1/tcp/8337@" .config/config.yml # 替换配置文件中的内容
-sleep 10 # 等待 10 秒
 GOEXPERIMENT=arenas go install ./... # 安装 Go 程序
-sleep 60 # 等待 1 分钟
 sudo bash -c 'echo -e "[Unit]\nDescription=Ceremony Client Go App Service\n\n[Service]\nType=simple\nRestart=always\nRestartSec=5s\nWorkingDirectory=/root/ceremonyclient/node\nEnvironment=GOEXPERIMENT=arenas\nExecStart=/root/go/bin/node ./...\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/ceremonyclient.service' # 创建 Systemd 服务文件
 systemctl start ceremonyclient # 启动 Systemd 服务
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest # 安装 grpcurl 工具

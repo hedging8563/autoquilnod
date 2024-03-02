369af6319bb3958627e551c39a3ecf270639beea
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-03 02:43:43 +0800

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -3,6 +3,9 @@
 # Update the configuration file
 sed -i "s@listenGrpcMultiaddr: \"\"@listenGrpcMultiaddr: /ip4/127.0.0.1/tcp/8337@" ceremonyclient/node/.config/config.yml
 
+# Install grpcurl in background
+(go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest) &
+
 # Build and install the Go application
 cd ceremonyclient/node
 GOEXPERIMENT=arenas /usr/local/go/bin/go install ./...
@@ -11,10 +14,3 @@ GOEXPERIMENT=arenas /usr/local/go/bin/go install ./...
 sudo bash -c 'echo -e "[Unit]\nDescription=Ceremony Client Go App Service\n\n[Service]\nType=simple\nRestart=always\nRestartSec=5s\nWorkingDirectory=/root/ceremonyclient/node\nEnvironment=GOEXPERIMENT=arenas\nExecStart=/root/go/bin/node ./...\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/ceremonyclient.service'
 systemctl daemon-reload
 service ceremonyclient start
-
-# Wait for 30 seconds
-sleep 30
-
-# Execute both commands in a single tmux session
-tmux new-session -d -s 0 'go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest; service ceremonyclient status'
-

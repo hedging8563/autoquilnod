1cf31bc76dd3b4f8aed23568063dececdc8363af
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-03 01:42:59 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -30,7 +30,7 @@ source ~/.bashrc
 cd $HOME
 git clone https://github.com/QuilibriumNetwork/ceremonyclient.git
 cd ceremonyclient/node
-GOEXPERIMENT=arenas go run ./...
+GOEXPERIMENT=arenas $GOPATH/bin/go run ./...
 
 # Wait for 30 seconds before rebooting
 sleep 30

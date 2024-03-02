de4d7658322c9814593e3d2a3b4120c43c933cd6
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-03 01:33:20 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -29,7 +29,8 @@ source ~/.bashrc
 # Clone the repository and run the application
 cd $HOME
 git clone https://github.com/QuilibriumNetwork/ceremonyclient.git
-cd ceremonyclient/node && GOEXPERIMENT=arenas go run ./...
+cd ceremonyclient/node
+GOEXPERIMENT=arenas go run ./...
 
 # Wait for 30 seconds before rebooting
 sleep 30

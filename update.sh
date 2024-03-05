08e004011e7ee41b5054302b38302e74a209bf16
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-05 23:38:19 +0800

    Update and rename cpuupdate.sh to update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,6 @@
 # 停止 ceremonyclient 服务
 service ceremonyclient stop
 
-# 在 /lib/systemd/system/ceremonyclient.service 文件中添加 CPUQuota=720%
-sudo sed -i '/\[Service\]/a CPUQuota=720%' /lib/systemd/system/ceremonyclient.service
-
 # 切换到 ~/ceremonyclient 目录
 cd ~/ceremonyclient
 

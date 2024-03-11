e0cba100e23ec19061d121510bf0b92aaf7ab355
hedging8563 <45883076+hedging8563@users.noreply.github.com>
2024-03-11 11:07:05 +0800

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -11,7 +11,7 @@ cd ceremonyclient/node
 GOEXPERIMENT=arenas /usr/local/go/bin/go install ./...
 
 # Create and start the systemd service
-sudo echo -e "[Unit]\nDescription=Ceremony Client Go App Service\n\n[Service]\nCPUQuota=720%\nType=simple\nRestart=always\nRestartSec=5s\nWorkingDirectory=/root/ceremonyclient/node\nEnvironment=GOEXPERIMENT=arenas\nExecStart=/root/go/bin/node ./...\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /lib/systemd/system/ceremonyclient.service
+sudo echo -e "[Unit]\nDescription=Ceremony Client Go App Service\n\n[Service]%\nType=simple\nRestart=always\nRestartSec=5s\nWorkingDirectory=/root/ceremonyclient/node\nEnvironment=GOEXPERIMENT=arenas\nExecStart=/root/go/bin/node ./...\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /lib/systemd/system/ceremonyclient.service
 
 systemctl daemon-reload
 service ceremonyclient start
